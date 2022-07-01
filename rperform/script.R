908f90a77342e6b1dde8b56228a336bd4554f357
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-07-01 12:34:15 +0000

    update num_commits to 10

--- rperform/script.R
+++ rperform/script.R
@@ -2,7 +2,7 @@
 # setwd("./")
 Rperform::plot_metrics(
     test_path = "inst/tests/test-join.r",
-    metric = "time", num_commits = 3, save_data = FALSE,
+    metric = "time", num_commits = 10, save_data = FALSE,
     save_plots = FALSE
 )
 # print("It is running the script")
