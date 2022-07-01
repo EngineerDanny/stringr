7d58bc4eac31f25489d31858c4c5f2c1803380ca
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-07-01 12:30:02 +0000

    script update

--- rperform/script.R
+++ rperform/script.R
@@ -1,8 +1,8 @@
 # the script should be written here to benchmark
-setwd("./")
+# setwd("./")
 Rperform::plot_metrics(
     test_path = "inst/tests/test-join.r",
-    metric = "time", num_commits = 1, save_data = FALSE,
+    metric = "time", num_commits = 3, save_data = FALSE,
     save_plots = FALSE
 )
-print("It is running the script")
+# print("It is running the script")
