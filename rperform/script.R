4ca31b470f9a6555061348357908b40f4def0b1a
Daniel Agyapong <agyapongdaniel7777@gmail.com>
2022-07-06 01:13:22 +0000

    Update script.R

--- rperform/script.R
+++ rperform/script.R
@@ -1,6 +1,6 @@
 # the script should be written here to benchmark
 # setwd("./")
- Rperform::mem_compare(
+ Rperform::time_compare(
     test_path = "inst/tests/test-dup.r",
     num_commits = 2, save_data = TRUE,
  )
