150ff40e74d9a169188d183fae7bc7463ab5f780
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-07-07 16:31:05 +0000

    update header.R

--- rperform/header.R
+++ rperform/header.R
@@ -1,13 +1,11 @@
 # You can modify the PR comment header here. You can use github markdown e.g.
 # emojis like :tada:.
-# This file will be parsed and evaluate within the context of
-# `benchmark_analyze` and should return the comment text as the last value.
-# Available variables for glue substitution:
-# * ci: confidence interval
-# * branches: BASE and HEAD branches benchmarked against each other.
-# See `?Rperform::pr_comment`
+
 glue::glue(
-  "This is how benchmark results would change (along with a",
-  " {100 * ci}% confidence interval in relative change) if ",
-  "{system2('git', c('rev-parse', 'HEAD'), stdout = TRUE)} is merged into {branches[1]}:\n"
+    "The table below represents the test results generated",
+    " after running `",
+    test_function,
+    "` function",
+    " on this PR branch",
+    "\n"
 )
