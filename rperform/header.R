9c61ba22e0f1fb5071ef9ee3fc0c4d7498707818
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-07-13 18:40:33 +0000

    update header

--- rperform/header.R
+++ rperform/header.R
@@ -2,13 +2,15 @@
 # emojis like :tada:.
 
 glue::glue(
-  "The table below represents the test results generated",
-  " after running `",
-  test_function,
-  "` function",
-  " on this PR branch",
-  "\n",
-  "\n <br/>",
-  image_url
+  "The image below represents the test results generated",
+    " after running `",
+    test_function,
+    "` function",
+    " on this PR branch.",
+    "Check out the test results data set in csv ",
+    csv_url,
+    "\n",
+    "<br/>",
+    image_url
 )
 
