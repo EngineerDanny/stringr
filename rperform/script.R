4e0a72689491fc6a33f42d0b13caa20f8ec903e3
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-07-13 04:02:17 +0000

    update script

--- rperform/script.R
+++ rperform/script.R
@@ -1,19 +1,19 @@
 # the script should be written here to benchmark
 # setwd("./")
  
-#  Rperform::plot_metrics(
-#      test_path = "inst/tests/test-join.r",
-#      metric = "time", num_commits = 5, save_data = TRUE,
-#      save_plots = TRUE
-#  )
-
- Rperform::plot_branchmetrics(
-    test_path = "inst/tests/test-interp.r", 
-    metric = "memory", 
-    branch1 = "rperform_test", 
-    branch2 = "master", 
-    save_data = F, 
-    save_plots = F
+ Rperform::plot_metrics(
+     test_path = "inst/tests/test-join.r",
+     metric = "time", num_commits = 5, save_data = TRUE,
+     save_plots = TRUE
  )
+
+#  Rperform::plot_branchmetrics(
+#     test_path = "inst/tests/test-interp.r", 
+#     metric = "memory", 
+#     branch1 = "rperform_test", 
+#     branch2 = "master", 
+#     save_data = F, 
+#     save_plots = F
+#  )
 
  
